a62db38daf16b8a4a78fdf16244f90ad3981e6c0
(Moved) <98882733+doktauwu@users.noreply.github.com>
2022-03-13 17:58:25 +0900

    Fixed Multi converting bug
    Fixed the bug that fails to convert mp4 to gif when tries to convert too many emoticons in same time

--- src/conDownloader.js
+++ src/conDownloader.js
@@ -16,11 +16,6 @@ const desktopDir = `${homeDir}/Desktop`;
 const conDownloader = async (emoticonPage, emoticonsTitle, convertProcessIndex) => {
     const { success, err = '', results } = await new Promise(
         (rootResolve, rootReject) => {
-            let gifConvertCount = {
-                "current": 0,
-                "target": 0
-            }
-
             // 사전 인자
 
             // const emoticonPage = "https://arca.live/e/19608?target=title&keyword=%EC%9D%B4%EC%84%B8%EB%8F%8C&p=1";
@@ -64,51 +59,64 @@ const conDownloader = async (emoticonPage, emoticonsTitle, convertProcessIndex) 
                 try { if (!fs.existsSync(savePath)) { fs.mkdirSync(savePath) } }
                 catch (err) { console.log(err) }
 
+                let currentVidIndex = 0
 
-                for (let i = 0; i < emoticons.length; i++) {
-                    const fileName = `${i}.${emoticons[i].split(".").pop()}`;
-                    downloadImage("https:" + emoticons[i], i, emoticons[i].split(".").pop(), savePath).then(() => {
-                        if (emoticons[i].split(".").pop() == "mp4") {
-                            const convertVidToGif = async () => {
-                                const { success, err = '', results } = await new Promise(
-                                    (resolve, reject) => {
-                                        gifConvertCount["target"] = gifConvertCount["target"] + 1
-                                        PythonShell.run("./resources/app/src/convert-to-gif.py", {
-                                            mode: "text",
-                                            pythonPath: "./resources/app/src/python/python.exe",
-                                            pythonOptions: ["-u"],
-                                            scriptPath: "",
-                                            args: [path.resolve(`${savePath}/${fileName}`), path.resolve(`${savePath}/${fileName.split(".").shift()}.gif`)]
-                                        }, (err, results) => {
-                                            if (err) {
-                                                gifConvertCount["current"] = gifConvertCount["current"] + 1
-                                                if (gifConvertCount["current"] == gifConvertCount["target"]) {
-                                                    rootResolve({ success: true, convertProcessIndex })
-                                                }
-                                                reject({ success: false, err })
-                                            };
-                                            // console.log(results)    
-                                            gifConvertCount["current"] = gifConvertCount["current"] + 1
-                                            if (gifConvertCount["current"] == gifConvertCount["target"]) {
-                                                rootResolve({ success: true, convertProcessIndex })
+                const processSingleEmoticon = async () => {
+                    const { success, err = '', results } = await new Promise(
+                        (processSingleEmoticonResolve, processSingleEmoticonReject) => {
+                            const fileName = `${currentVidIndex}.${emoticons[currentVidIndex].split(".").pop()}`;
+                            downloadImage("https:" + emoticons[currentVidIndex], currentVidIndex, emoticons[currentVidIndex].split(".").pop(), savePath).then(() => {
+                                if (emoticons[currentVidIndex].split(".").pop() == "mp4") {
+                                    const convertVidToGif = async () => {
+                                        const { success, err = '', results } = await new Promise(
+                                            (resolve, reject) => {
+                                                // !!FOR DEBUG!!    PythonShell.run("./src/convert-to-gif.py", {
+                                                PythonShell.run("./resources/app/src/convert-to-gif.py", {
+                                                    mode: "text",
+                                                    // !!FOR DEBUG!!     pythonPath: "./src/python/python.exe",
+                                                    pythonPath: "./resources/app/src/python/python.exe",
+                                                    pythonOptions: ["-u"],
+                                                    scriptPath: "",
+                                                    args: [path.resolve(`${savePath}/${fileName}`), path.resolve(`${savePath}/${fileName.split(".").shift()}.gif`)]
+                                                }, (err, results) => {
+                                                    if (err) {
+                                                        reject({ success: false, err })
+                                                    };
+                                                    // console.log(results)    
+                                                    resolve({ success: true, results })
+                                                })
                                             }
-                                            resolve({ success: true, results })
-                                        })
+                                        );
                                     }
-                                );
-                            }
-                            convertVidToGif().then(() => { }).then(() => {
-                                fs.unlink(`${savePath}/${fileName}`, () => { })
-                            }).catch(() => {
-                                console.log("converting error occurred");
-
+                                    convertVidToGif().then(() => { }).then(() => {
+                                        fs.unlink(`${savePath}/${fileName}`, () => { });
+                                        processSingleEmoticonResolve({ success: true });
+                                    }).catch(() => {
+                                        console.log("converting error occurred");
+                                        processSingleEmoticonResolve({ success: false });
+                                    })
+                                }
+                                processSingleEmoticonResolve({ success: false });
                             })
-                        }
-                        if (emoticons.filter(x => x.split(".").pop() == "mp4").length < 1) {
-                            rootResolve({ success: true, convertProcessIndex })
-                        }
-                    })
+                        })
                 }
+
+
+
+
+                const asyncDownloadHandler = async () => {
+                    const { success, err = '', results } = await new Promise(
+                        (resolve, reject) => {
+
+                            if (emoticons.length - 1 >= currentVidIndex) {
+                                processSingleEmoticon().then(() => { currentVidIndex++; asyncDownloadHandler(); resolve({ success: true }) })
+                            }
+                            else {
+                                rootResolve({ success: true, convertProcessIndex })
+                            }
+                        })
+                }
+                asyncDownloadHandler()
             }).catch(err => console.log(err))
         })
 }
